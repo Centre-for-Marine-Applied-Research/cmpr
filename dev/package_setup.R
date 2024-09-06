cbc0f725bb8c39a3536bc348b941cb7f4eeeaf5d
Rachel Woodside <rachel.woodside3@gmail.com>
2024-09-06 08:55:51 -0300

    Set up folder to track code for package creation

---
+++ dev/package_setup.R
@@ -0,0 +1,13 @@
+library(devtools)
+
+# set up project
+#create_package("path/to/package/folder")
+
+# add license
+use_gpl3_license()
+
+# set up git repo
+use_git()
+
+# set up github
+use_github()

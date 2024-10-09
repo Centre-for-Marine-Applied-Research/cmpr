d955215dcfdca80ccf6c874e9b72d714b397d447
rachelwoodside <rachel.woodside3@gmail.com>
2024-10-09 11:19:47 -0300

    Convert DB connection to function and document

--- R/connect_to_db.R
+++ R/connect_to_db.R
@@ -1,16 +1,27 @@
-library(keyring)
-library(DBI)
+#' Connect to CMP Database
+#'
+#' @param connection_config name of configuration in config.yml to use for connection
+#'
+#' @return database connection object
+#' @import config
+#' @import RPostgres
+#' @importFrom DBI dbConnect
+#' @export
+#'
+#' @examples
+#' db_conn <- try(connect_to_db())
+connect_to_db <- function(connection_config = "default") {
+  # Get configuration file with database connection details
+  db_config <- config::get(config = connection_config)
 
-# Get configuration file with database connection details
-db_config <- config::get(config = "default")
-
-# Initialize database connection
-con <- dbConnect(RPostgres::Postgres(),
-                 user = db_config$user,
-                 password = db_config$password,
-                 host = db_config$host,
-                 port = db_config$port,
-                 dbname = db_config$dbname)
-
+  # Initialize database connection
+  conn <- DBI::dbConnect(RPostgres::Postgres(),
+                   user = db_config$user,
+                   password = db_config$password,
+                   host = db_config$host,
+                   port = db_config$port,
+                   dbname = db_config$dbname)
+}
 # Close database connection
-#dbDisconnect(con)
+#DBI::dbDisconnect(con)
+

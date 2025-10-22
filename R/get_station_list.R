856f6b3f2e5f07558d2508095fde0e4f3d1fd88a
rachelwoodside <rachel.woodside3@gmail.com>
2025-10-22 09:54:57 -0300

    Update documentation for get_station_list.R

--- R/get_station_list.R
+++ R/get_station_list.R
@@ -1,3 +1,10 @@
+#' Get list of CMP stations
+#'
+#' @param conn database connection object
+#'
+#' @returns data frame of all stations and their attributes
+#' @export
+#'
 get_station_list <- function(conn) {
   res <- dbSendQuery(conn, "SELECT * FROM sensorstring.ss_station;")
   station_table <- dbFetch(res)

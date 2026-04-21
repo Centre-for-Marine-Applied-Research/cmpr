e34666c7df2622d329a421cf02358446c7c639aa
Rachel Woodside <rachel.woodside3@gmail.com>
2026-04-21 11:36:32 -0300

    Update cmp_get_depl_data, run devtools check
    Fix roxygen docs and allow variable filtering options

--- R/cmpr_get_depl_data.R
+++ R/cmpr_get_depl_data.R
@@ -1,8 +1,12 @@
-#' Get list of CMP stations
+#' Get measurement data from a specific CMP deployment
 #'
 #' @param conn database connection object
+#' @param station_name name of the station where the relevant deployment is located
+#' @param depl_date deployment date of the station
+#' @param variable_type type of variable to filter the data for, excludes use of variable_name
+#' @param variable_name name of variable to filter the data for, excludes use of variable_type
 #'
-#' @returns data frame of all stations and their attributes
+#' @returns data frame of data measured over the course of a deployment
 #' @export
 #'
 #' @importFrom glue glue
@@ -25,51 +29,63 @@ cmpr_get_depl_data <- function(
   }
   # TODO: ensure date parses correctly
   # TODO: ensure variable type is valid
-  # TODO: make this work with some arguments as optional (this will require changing the query structure)
-  # Suggestion - separate out the SelectedVariable CTE to be constructed separately, then pasted in?
-  # Construct CTE for selected variable type or name
-  # if (is.null(variable_type) & is.null(variable_name)) {
-  #   selected_var_cte <- NULL
-  # } else {
-  #   var_selection <- is.null(variable_type)?variable_name:variable_type
-  #   var_selection_col <- rlang::as_string(var_selection)
-  #   selected_var_cte <- glue::glue(
-  #     ", SelectedVariable AS (
-  #     SELECT *
-  #     FROM sensorstring.ss_variable
-  #     WHERE {var_selection} = '{var_selection}')"
-  #   )
-  # }
-  res <- DBI::dbSendQuery(
-    conn,
-    paste0(
-      "WITH SelectedDeployment AS (
+
+  # Construct CTE for selected deployment
+  selected_depl_cte <- glue::glue(
+    "SelectedDeployment AS (
       SELECT ss_station.station_id, ss_station.station_name, ss_depl.depl_id, depl_date, retrieval_date, sensor_depth_m
       FROM sensorstring.ss_depl
       LEFT JOIN sensorstring.ss_station
       ON ss_depl.station_id = ss_station.station_id
       LEFT JOIN sensorstring.sensor_depl
       ON ss_depl.depl_id = sensor_depl.depl_id
-      WHERE station_name = '",
-      station_name,
-      "' AND depl_date = '",
-      depl_date,
-      "'
-      ), SelectedVariable AS (
-        SELECT *
-        FROM sensorstring.ss_variable
-        WHERE variable_type = '",
-      variable_type,
-      "'
-      )
-      SELECT station_name, depl_date, retrieval_date, sensor_serial_num, sensor_depth_m, variable_type, variable_name 
+      WHERE station_name = '{station_name}'",
+    " AND depl_date = '{depl_date}')"
+  )
+
+  # Construct CTE for selected variable type or name
+  if (is.null(variable_type) & is.null(variable_name)) {
+    selected_var_cte <- glue::glue(
+      ", SelectedVariable AS (
+      SELECT *
+      FROM sensorstring.ss_variable)"
+    )
+  } else {
+    if (is.null(variable_type)) {
+      var_selection_val <- variable_name
+      var_selection_col <- "variable_name"
+    } else {
+      var_selection_val <- variable_type
+      var_selection_col <- "variable_type"
+    }
+    selected_var_cte <- glue::glue(
+      ", SelectedVariable AS (
+      SELECT *
+      FROM sensorstring.ss_variable
+      WHERE {var_selection_col} = '{var_selection_val}')"
+    )
+  }
+
+  # Queries are constructed using paste0 since sending them to the DB requires a "string" object not a "glue" object
+  query <-
+    paste0(
+      "WITH ",
+      selected_depl_cte,
+      selected_var_cte,
+      "SELECT station_name, depl_date, retrieval_date, sensor_serial_num, sensor_depth_m, variable_type, variable_name 
       FROM SelectedDeployment
       LEFT JOIN sensorstring.sensor_depl_measurement
       ON SelectedDeployment.depl_id = sensor_depl_measurement.depl_id
       RIGHT JOIN SelectedVariable
       ON SelectedVariable.variable_id = sensor_depl_measurement.variable_id;"
     )
+
+  # TODO: Implement pagination and a progress bar
+  res <- DBI::dbSendQuery(
+    conn,
+    query
   )
+
   depl_table <- DBI::dbFetch(res)
   DBI::dbClearResult(res)
 

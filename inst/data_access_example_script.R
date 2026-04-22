4d565d9b206fc2da361e932298d9a6a8ead44a4c
Rachel Woodside <rachel.woodside3@gmail.com>
2026-04-22 11:57:46 -0300

    Update data_access_example_script.R

--- inst/data_access_example_script.R
+++ inst/data_access_example_script.R
@@ -4,23 +4,32 @@ devtools::load_all()
 conn <- cmpr_connect_to_db(connection_config = "admin")
 
 # Look at all stations and their location data
-station_metadata <- cmpr_get_station_data(conn)
+# TODO: Look at all station data overall
+# TODO: Accept vector of stations
+station_metadata <- cmpr_get_station_metadata(conn)
 
 # Look at all the deployments at a station
-depl_metadata <- cmpr_get_station_depl(conn, station_name = "Wedgeport")
+station_depls <- cmpr_get_station_depl(conn, station_name = "Jewers Bay")
 # TODO: Add option to get retrieval data and other log details
 
-# TODO: Look at the variables available at different stations
+# TODO (low priority): Look at the variables available at different stations
 # Avoid going via the measurements since there are so many rows
 # Instead go via sensor -> sensor_model -> sensor_model_variable -> ss_variable
 
-# TODO
+# TODO (low priority):
 # Look at all the sensors in a given deployment
 # sensor_metadata <- cmpr_get_depl_sensors(
 #   conn,
 #   station_name = "Wedgeport",
 #   depl_date = "2020-06-16"
 # )
+
+# Get all the data from a given station
+station_data <- cmpr_get_station_data(
+  conn,
+  station_name = "Wedgeport",
+  variable_type = "temperature"
+)
 
 # Get all the data from a specific deployment
 # (options for specifying which sensors or variables whether to include qc flags)

533d20a1359c25582a159068d1c6322d0408abe1
Rachel Woodside <rachel.woodside3@gmail.com>
2026-04-28 16:10:33 -0300

    Depl data timestamp inclusion fix

--- R/cmpr_get_depl_data.R
+++ R/cmpr_get_depl_data.R
@@ -73,7 +73,7 @@ cmpr_get_depl_data <- function(
       "WITH ",
       selected_depl_cte,
       selected_var_cte,
-      "SELECT station_name, depl_date, retrieval_date, sensor_serial_num, sensor_depth_m, variable_type, variable_name, variable_value
+      "SELECT station_name, depl_date, retrieval_date, sensor_serial_num, sensor_depth_m, timestamp_utc, variable_type, variable_name, variable_value
       FROM SelectedDeployment
       LEFT JOIN sensorstring.sensor_depl_measurement
       ON SelectedDeployment.depl_id = sensor_depl_measurement.depl_id
